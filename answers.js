f2b4d0af8df352157d5eab012b88a8ffca06efa2
Aurélien Roux <aureroux13@me.com>
2017-01-10 19:14:18 +0000

    second answer

--- answers.js
+++ answers.js
@@ -10,3 +10,14 @@ function firstChar(word) {
 
 console.log(firstChar("decode"));
 console.log(firstChar("Montreal"));
+
+//Write a function that takes a string and returns the last character of a string. 
+//Test your function on a few inputs, including the empty string.
+
+function lastChar(word){
+    word = word.split("");
+    return word[word.length - 1];
+}
+
+console.log(lastChar("decode"));
+console.log(lastChar("Montreal"));
