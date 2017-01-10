cec5cc4268de12e9a4de906f05d26249a8e71dfd
Aurélien Roux <aureroux13@me.com>
2017-01-10 19:09:22 +0000

    first answer

--- answers.js
+++ answers.js
@@ -1,1 +1,12 @@
 //this is a comment 
+
+//Write a function that takes a string and returns the first character of the string. 
+//Test your function on a few inputs, including the empty string.
+
+function firstChar(word) {
+    word = word.split("");
+    return word[0];
+}
+
+console.log(firstChar("decode"));
+console.log(firstChar("Montreal"));
