cf9ec37b6efc6dfbe49b8c1502ea7ae8344e236f
Aurélien Roux <aureroux13@me.com>
2017-01-10 19:18:02 +0000

    forgot the empty string test

--- answers.js
+++ answers.js
@@ -10,6 +10,7 @@ function firstChar(word) {
 
 console.log(firstChar("decode"));
 console.log(firstChar("Montreal"));
+console.log(firstChar(""));
 
 //Write a function that takes a string and returns the last character of a string. 
 //Test your function on a few inputs, including the empty string.
@@ -21,3 +22,10 @@ function lastChar(word){
 
 console.log(lastChar("decode"));
 console.log(lastChar("Montreal"));
+console.log(firstChar(""));
+
+//Write a function that takes a string and a number, 
+//and returns the character at the position represented by the number. 
+//The indexing of number should start at 0. 
+//Test your function on a few inputs, including the empty string.
+
