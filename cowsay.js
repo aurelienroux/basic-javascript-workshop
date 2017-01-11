73ebc528ea6327ec4d25d39420bb37dcfc264614
Aurélien Roux <aureroux13@me.com>
2017-01-11 04:09:32 +0000

    tried and falied to install cowsay

---
+++ cowsay.js
@@ -0,0 +1,9 @@
+// The Cow Whisperer
+
+// Challenge: create a file called cowsay.js in your Cloud9 project. 
+// Running this file should output a cow saying a random fortune. 
+// This is reproducing the cowsay command-line app. Make sure 
+// to add/commit this file and push it to your GitHub. 
+// If the fortune is longer than 30 characters, you have to wrap it 
+// on a new line. For extra brownie points, wrap your fortune at 
+// the word level instead of the letter level.
