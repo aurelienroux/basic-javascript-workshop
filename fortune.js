5ed182f36e34e0bdb7c9f5e155b874fe3d9c5bd1
Aurélien Roux <aureroux13@me.com>
2017-01-11 03:46:23 +0000

    deleted answers and created fortune

---
+++ fortune.js
@@ -0,0 +1,5 @@
+// The Fortune Teller
+
+// Challenge: create a file called fortune.js in your Cloud9 project. 
+// Running this file should output a random quote from a list of 10 different 
+// quotes. Make sure to add/commit this file and push it to your GitHub.
