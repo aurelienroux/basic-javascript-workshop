e64db44a5b59c9a1628aab4071a54e68054030ff
Aurélien Roux <aureroux13@me.com>
2017-01-14 22:10:56 +0000

    refactor unique numbers array

--- answers.js
+++ answers.js
@@ -1,28 +1,34 @@
-//this is a comment 
+// Write a function that takes a string and returns the first character of the string. 
+// Test your function on a few inputs, including the empty string.
 
-//Write a function that takes a string and returns the first character of the string. 
-//Test your function on a few inputs, including the empty string.
-
-function firstChar(word) {
-    word = word.split("");
-    return word[0];
-}
+// function firstChar(word) {
+//     word = word.split("");
+//     return word[0];
+// }
 
 // console.log(firstChar("decode"));
 // console.log(firstChar("Montreal"));
 // console.log(firstChar(""));
 
+
+
+
+
 //Write a function that takes a string and returns the last character of a string. 
 //Test your function on a few inputs, including the empty string.
 
-function lastChar(word){
-    word = word.split("");
-    return word[word.length - 1];
-}
+// function lastChar(word){
+//     word = word.split("");
+//     return word[word.length - 1];
+// }
 
 // console.log(lastChar("decode"));
 // console.log(lastChar("Montreal"));
-// console.log(firstChar(""));
+// console.log(lastChar(""));
+
+
+
+
 
 //Write a function that takes a string and a number, 
 //and returns the character at the position represented by the number. 
@@ -37,6 +43,9 @@ function lastChar(word){
 // console.log(locate("Montreal", 4));
 // console.log(locate(""));
 
+
+
+
 //Write a function that takes two numbers and adds them together. Test your function on a few inputs. 
 //Write in the comments what happens when you pass something other than a number to your function.
 
@@ -50,6 +59,11 @@ function lastChar(word){
 // //if Nan, the function concatenates the two inputs
 // console.log(add("text", "num"));
 
+
+
+
+
+
 //Write a function that takes two numbers and multiplies them together. 
 //Test your function on a few inputs. 
 //Write in your comments what happens when you pass something other than a number to your function.
@@ -62,6 +76,9 @@ function lastChar(word){
 // console.log(multiply(5,10));
 // //function return Nan
 // console.log(multiply("text", "num"));
+
+
+
 
 
 //Write a function that takes two numbers and a string. 
@@ -89,6 +106,10 @@ function lastChar(word){
 // console.log(numberOp("mult", 3, 4));
 // console.log(numberOp("div", 8, 4));
 
+
+
+
+
 //Write a function that takes a string and a number, 
 //and returns the string repeated that many number of times. 
 //Test your function with various inputs.
@@ -110,6 +131,10 @@ function lastChar(word){
 // }
 
 // oneToTen();
+
+
+
+
 
 //Write a function that uses a for loop to print the numbers from 1 to 10 inclusively, 
 //but as words ("one", "two", ...). As an extra challenge, 
@@ -143,11 +168,49 @@ function lastChar(word){
 
 // countFunc();
 
+// //extra challenge
+// function repeat (){
+//     for(var i = 1; i < 11; i++){
+//         switch (i) {
+//             case 1:
+//                 console.log("one");
+//                 break;
+//             case 2:
+//                 console.log("two");
+//                 break;
+//             case 3:
+//                 console.log("three");
+//                 break;
+//             case 4:
+//                 console.log("four");
+//                 break;
+//             case 5:
+//                 console.log("five");
+//                 break;
+//             case 6:
+//                 console.log("six");
+//                 break;
+//             case 7:
+//                 console.log("seven");
+//                 break;
+//             case 8:
+//                 console.log("eight");
+//                 break;
+//             case 9:
+//                 console.log("nine");
+//                 break;
+//             default:
+//                 console.log("ten");
+//         }
+//     }
+// }
+// repeat();
+
+
 //Write a function that uses a for loop to print the numbers from 1 to 10 inclusively, 
 //but for each number, print it that many number of times:
 
 // function loopNum(){
-    
 //     for(var i = 1; i < 11; i++){
 //         for(var j = 0; j < i; j++){
 //             console.log(i);
@@ -156,6 +219,11 @@ function lastChar(word){
 // }
 
 // loopNum();
+
+
+
+
+
 
 //Write a function that takes a string, and returns the reverse of that string. 
 //For example, if you pass the function the string “hello”, it should return “olleh”. 
@@ -168,6 +236,11 @@ function lastChar(word){
 // console.log(reverseStr("montreal"));
 // console.log(reverseStr("Decode"));
 // console.log(reverseStr(""));
+
+
+
+
+
 
 
 //Write a function that takes a number and returns the factorial of a number. 
@@ -187,6 +260,10 @@ function lastChar(word){
 // console.log(factor(0));
 // console.log(factor(-5));
 
+
+
+
+
 //Write a function that takes a phrase as a string, and returns the longest word 
 //in that phrase. If the phrase contains more than one such word, 
 //return the first occurrence. Test your function on a few inputs.
@@ -195,7 +272,6 @@ function lastChar(word){
 //     var tmp = sentence.split(" ");
 //     var fin = 0;
 //     var sol;
-    
 //     for(var i = 0; i < tmp.length - 1; i++){
 //         if(tmp[i].length > fin ){
 //             fin = tmp[i].length;
@@ -208,6 +284,19 @@ function lastChar(word){
 // console.log(longWord("hello universe this is a sentence"));
 // console.log(longWord("hello world this is a word"));
 // console.log(longWord("hi country this is a number"));
+
+// //REFACTOR*******************************************************
+// function longWord(str){
+//     var sol = str.split(" ").sort( (a, b) => b.length - a.length);
+//     return sol[0];
+// }
+
+// console.log(longWord("this is a sentence"));
+// console.log(longWord("hello universe this is a sentence"));
+// console.log(longWord("hello world this is a word"));
+// console.log(longWord("hi country this is a number"));
+
+
 
 //Write a function that takes a phrase, and returns the same phrase with 
 //every word capitalized. For example, if you pass your function "hello world", 
@@ -223,12 +312,28 @@ function lastChar(word){
 //     console.log(sent = sent.join(" "));
 // }
 
-// // console.log(capitalSent("HellO WorLd this iS a seNtEnce"));
 // capitalSent("HellO WorLd this iS a seNtEnce");
 // capitalSent("HellO uNIverSe this iS a WOrd");
 // capitalSent("HellO CouNtRy this iS a NumBEr");
 
-//Write a function that takes an array and returns the largest number of the array. 
+// REFACTOR****************************************
+// function capSent(str){
+//     str = str.toLowerCase().split(" ");
+//     var strfinal = str.map(function(word){
+//         return word[0].toUpperCase() + word.slice(1);
+//     })
+//     return strfinal = strfinal.join(" ");
+// }
+
+// console.log(capSent("this is a sentence"));
+// console.log(capSent("hello universe this is a sentence"));
+// console.log(capSent("hello world this is a word"));
+// console.log(capSent("hi country this is a number"));
+
+
+
+//Write a function that takes an array and returns 
+//the largest number of the array. 
 //Test your function on a few inputs.
 
 // function maximum(arr){
@@ -238,14 +343,18 @@ function lastChar(word){
 //     console.log(arr[0]);
 // }
 
-// // console.log(maximum([12, 34, 56, 78]));
 // maximum([12, 34, 56, 78]);
 // maximum([12, 3334, 526, 7]);
 // maximum([129, 434, 5556, 738]);
 
-//Write a function that takes an array, and returns a filtered array. 
-//The filtered array should only contain the truthy values from the initial array. 
-//Hint: there is an array method called filter that can help you with this :)
+
+
+
+// //Write a function that takes an array, and returns a 
+// filtered array. The filtered array should only contain 
+// the truthy values from the initial array. Hint: there 
+// is an array method called filter that can help you 
+// with this :)
 
 // function truth(arr){
 //     arr = arr.filter(function(item){
@@ -256,6 +365,16 @@ function lastChar(word){
 
 // console.log(truth([false, null, undefined, NaN, "Aurelien", -8, 0, true, 1, 19]))
 
+//REFACTOR***************************************************
+// function func(arr){
+//     return arr.filter((item) => Boolean(item) == true);
+// }
+
+// console.log(func([false, null, undefined, NaN, "Aurelien", -8, 0, true, 1, 19]));
+
+
+
+
 //Write a function that takes an array of numbers, 
 //and returns the sum of all the numbers in the array.
 
@@ -265,15 +384,23 @@ function lastChar(word){
 
 // console.log(sumAll([12, 23, 34, 45]));
 
+// //REFACTOR **********************************************
+// function sumAll(arr){
+//     return arr.reduce( (a,b) => a + b);
+// }
+
+// console.log(sumAll([12, 23, 34, 45]));
+
+
+
+
 //Write a function that takes two arrays, and returns an array of all elements 
 //that are only in one array. For example, with [1,2,3] and [1,2,4,5] the function 
 //should return [3,4,5]. Test your function on different inputs. Hint: you should 
 //look up array methods indexOf and slice.
 
-
 // function twoArr(a, b){
 //     var tmp = [];
-
 //     for(var i = 0; i < a.length; i++){
 //         for (var j = 0; j < b.length; j++){
 //             if( a[i] === b[j] ){
@@ -281,7 +408,6 @@ function lastChar(word){
 //             }
 //         }
 //     }
-    
 //     for(var i = 0; i < tmp.length; i++){
 //         a.splice(a.indexOf(tmp[i]),1);
 //         b.splice(b.indexOf(tmp[i]),1);
@@ -290,6 +416,22 @@ function lastChar(word){
 // }
 
 // console.log(twoArr([1,2,3], [1,2,4,5]));
+
+/***************************************/
+
+//REFACTOR *****************************************
+// function twoArr(arr1, arr2){
+//     var arr3 = arr1.filter( (ele) => arr2.indexOf(ele) == -1);
+//     var arr4 = arr2.filter( (ele) => arr1.indexOf(ele) == -1);
+//     return arr3.concat(arr4);
+// }
+
+// console.log(twoArr([1,2,3], [1,2,4,5]));
+
+
+
+
+
 
 //Mini Challenge: write a function that takes an array and a function 
 //as arguments. The function should return a new array that maps every 
